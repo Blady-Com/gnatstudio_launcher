0891fc34e2a4c58adf9304a9fd9636bf336982d5
Blady <blady.net@orange.fr>
2024-06-22 21:43:50 +0200

    Add gnatstudio_launcher.rc support to defined extra variables.

--- src/gnatstudio_launcher.adb
+++ src/gnatstudio_launcher.adb
@@ -1,13 +1,17 @@
-with GNAT.OS_Lib;               use GNAT.OS_Lib;
+with GNAT.OS_Lib; use GNAT.OS_Lib;
 with Ada.Environment_Variables; use Ada.Environment_Variables;
-with Ada.Text_IO;               use Ada.Text_IO;
-with Ada.Command_Line;          use Ada.Command_Line;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Strings.Fixed;
+with Ada.Containers.Indefinite_Ordered_Maps;
+with Ada.Directories;
 
 procedure GNATStudio_Launcher is
 
-   function Append_If_Exists (PATH_Name : String) return String;
-   procedure Save_And_Set (Name, Val : String);
+   package Var_RC is new Ada.Containers.Indefinite_Ordered_Maps (String, String);
+   Var_RC_map : Var_RC.Map;
+   use type Var_RC.Cursor;
+
    function Append_If_Exists (PATH_Name : String) return String is
    begin
       if Exists (PATH_Name) then
@@ -16,6 +20,7 @@ procedure GNATStudio_Launcher is
          return "";
       end if;
    end Append_If_Exists;
+
    procedure Save_And_Set (Name, Val : String) is
       Backup_Name : constant String := "GPS_STARTUP_" & Name;
    begin
@@ -27,17 +32,58 @@ procedure GNATStudio_Launcher is
       Set (Name, Val);
    end Save_And_Set;
 
-   AL              : Argument_List (1 .. Argument_Count);
+   procedure Parse_Var_RC is
+      GSLRC : constant String := "/.gnatstudio/gnatstudio_launcher.rc";
+      Var_RC_File : File_Type;
+   begin
+      if Exists ("GNATSTUDIO_HOME") and then Ada.Directories.Exists (Value ("GNATSTUDIO_HOME") & GSLRC) then
+         Put_Line ("Found: " & Value ("GNATSTUDIO_HOME") & GSLRC);
+         Open (Var_RC_File, In_File, Value ("GNATSTUDIO_HOME") & GSLRC);
+      elsif Ada.Directories.Exists (Value ("HOME") & GSLRC) then
+         Put_Line ("Found: " & Value ("HOME") & GSLRC);
+         Open (Var_RC_File, In_File, Value ("HOME") & GSLRC);
+      else
+         Put_Line ("No gnatstudio_launcher.rc file found.");
+         return;
+      end if;
+      while not End_Of_File (Var_RC_File) loop
+         declare
+            Line : constant String := Get_Line (Var_RC_File);
+            use Ada.Strings.Fixed;
+         begin
+            if Line'Length /= 0 and then Line (1) /= '#' then
+               if Index (Line, "=") /= 0 then
+                  Var_RC_map.Insert (Line (1 .. Index (Line, "=") - 1), Line (Index (Line, "=") + 1 .. Line'Last));
+                  Put_Line
+                    ("Found: " & Line (1 .. Index (Line, "=") - 1) & '=' & Line (Index (Line, "=") + 1 .. Line'Last));
+               end if;
+            end if;
+         end;
+      end loop;
+      Close (Var_RC_File);
+   end Parse_Var_RC;
+
+   procedure Set_Var_RC is
+      use Var_RC;
+   begin
+      for C in Var_RC_map.Iterate loop
+         if Exists (Key (C)) then
+            Put_Line ("Append " & Key (C) & " with: " & Element (C));
+            Set (Key (C), Value (Key (C)) & ':' & Element (C));
+         else
+            Put_Line ("Create " & Key (C) & " with: " & Element (C));
+            Set (Key (C), Element (C));
+         end if;
+      end loop;
+   end Set_Var_RC;
+
+   AL : Argument_List (1 .. Argument_Count);
    Launcher_Ending : constant String := "_launcher";
-   App_Loc         : constant String := "/MacOS";
-   Launcher_Loc    : constant String := Command_Name;
-   Exe_Loc         : constant String :=
-     Launcher_Loc
-       (Launcher_Loc'First .. Launcher_Loc'Last - Launcher_Ending'Length);
+   App_Loc : constant String := "/MacOS";
+   Launcher_Loc : constant String := Command_Name;
+   Exe_Loc : constant String := Launcher_Loc (Launcher_Loc'First .. Launcher_Loc'Last - Launcher_Ending'Length);
    Res_Loc : constant String :=
-     Exe_Loc
-       (Exe_Loc'First .. Ada.Strings.Fixed.Index (Exe_Loc, App_Loc) - 1) &
-     "/Resources";
+     Exe_Loc (Exe_Loc'First .. Ada.Strings.Fixed.Index (Exe_Loc, App_Loc) - 1) & "/Resources";
 
 begin
    Put_Line ("Launcher: " & Launcher_Loc);
@@ -45,20 +91,20 @@ begin
    Put_Line ("Res     : " & Res_Loc);
    Put_Line ("GS_GNAT_PATH: " & Append_If_Exists ("GS_GNAT_PATH"));
    Put_Line ("GS_GPR_PATH : " & Append_If_Exists ("GS_GPR_PATH"));
+   Parse_Var_RC;
 
    Set ("GPS_ROOT", Res_Loc);
    Set ("GNATSTUDIO_PYTHONHOME", Res_Loc);
    Save_And_Set ("XDG_DATA_DIRS", Res_Loc & "/share");
    Save_And_Set ("GTK_EXE_PREFIX", Res_Loc);
-   Save_And_Set
-     ("GDK_PIXBUF_MODULE_FILE",
-      Res_Loc & "/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache");
+   Save_And_Set ("GDK_PIXBUF_MODULE_FILE", Res_Loc & "/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache");
    Save_And_Set ("GI_TYPELIB_PATH", Res_Loc & "/lib/girepository-1.0");
-   Set
-     ("PATH",
-      Value ("PATH") & Append_If_Exists ("GS_GNAT_PATH") &
-      Append_If_Exists ("GS_GPR_PATH"));
+   if Var_RC_map.Find ("PATH") = Var_RC.No_Element then
+      Put_Line ("Append PATH with: " & Append_If_Exists ("GS_GNAT_PATH") & Append_If_Exists ("GS_GPR_PATH"));
+      Set ("PATH", Value ("PATH") & Append_If_Exists ("GS_GNAT_PATH") & Append_If_Exists ("GS_GPR_PATH"));
+   end if;
    Set ("DYLD_LIBRARY_PATH", Res_Loc & "/lib:" & Res_Loc & "/lib/gdk-pixbuf-2.0/2.10.0/loaders");
+   Set_Var_RC;
 
    for Ind in AL'Range loop
       AL (Ind) := new String'(Argument (Ind));
